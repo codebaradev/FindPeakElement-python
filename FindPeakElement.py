c5c8c44f76d225950ccd8309d410cb7ee7696e1c
codebaradev <bahnasep@gmail.com>
2025-03-28 21:59:29 +0800

    Complete

--- FindPeakElement.py
+++ FindPeakElement.py
@@ -2,7 +2,7 @@ import time
 import random
 
 
-# BF = Brute Force (Sequence)
+# BF = Brute Force
 def findPeakBF(arr):
     for current in range(len(arr)):
         if (current == 0 and arr[current] > arr[current + 1]):
@@ -14,7 +14,7 @@ def findPeakBF(arr):
         if (current > 0 and arr[current] > arr[current + 1] and arr[current] > arr[current - 1]):
             return arr[current]
 
-# DC = Divide and Conquer (Binary Search)
+# DC = Divide and Conquer
 def findPeakDC(arr):
     left = 0
     right = len(arr) - 1
@@ -32,18 +32,22 @@ def findPeakDC(arr):
 def main():
     arrs = [
         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7],
-        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
-        [30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 35, 36, 37, 38, 37, 32],
+        [2, 4, 5, 9, 10, 13, 17, 19, 22, 25, 27, 31, 33, 35, 36, 39, 40, 41, 42, 44, 47, 50, 44, 30, 6, 5, 4, 3, 2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 11, 9, 6, 4, 2, 1, 3, 4, 10]
     ]
 
     # Brute Force
+    print("\n*** Peak Finding Problem ***\n")
     print("Brute Force")
     for arr in arrs:
         start_time = time.perf_counter()
         peak = findPeakBF(arr) 
         end_time = time.perf_counter()
+
         execution_time = end_time - start_time
         print(f"Peak: {peak} -> {execution_time:.6f} seconds")
+
+    print()
         
     # Divide and Conquer
     print("Divide and Conquer")
@@ -51,6 +55,7 @@ def main():
         start_time = time.perf_counter()
         peak = findPeakDC(arr) 
         end_time = time.perf_counter()
+
         execution_time = end_time - start_time
         print(f"Peak: {peak} -> {execution_time:.6f} seconds")
 
--- README.md
+++ README.md
@@ -7,15 +7,36 @@ Algorithm FindPeakElement(arr)
     right ← length of arr - 1
     
     while left <= right then
+
         mid ← left + ((right - left) // 2)
-        
-        if mid > 0 and arr[mid] < arr[mid - 1] then
-            right ← mid - 1
+                
+        if mid > 0 and arr[mid] < arr[mid - 1] then 
+            right ← mid - 1 
         else if mid < length of arr - 1 and arr[mid] < arr[mid + 1] then
             left ← mid + 1
         else
             return arr[mid]
 ``` 
 
+```pseudocode
+Algorithm FindPeakElement(arr)
+    // Menetapkan posisi kiri(elemen Pertama) dan kanan(elemen terakhir)
+    left ← 0
+    right ← length of arr - 1
+    
+    // Melakukan perulangan sampai left = right
+    while left <= right then
+
+        // Menentukan elemen tengah
+        mid ← left + ((right - left) // 2)
+                
+        if mid > 0 and arr[mid] < arr[mid - 1] then // 
+            right ← mid - 1 // elemen disebelah kiri elemen tengah akan menjadi elemen kanan
+            // dengan kata lain disisi kiri elemen tengah terdapat peak
+        else if mid < length of arr - 1 and arr[mid] < arr[mid + 1] then
+            left ← mid + 1
+        else
+            return arr[mid]
+``` 
 Reference:
 * [Find Peak Element - Leetcode 162 - Python](https://www.youtube.com/watch?v=kMzJy9es7Hc)
